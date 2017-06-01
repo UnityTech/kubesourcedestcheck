4cd0d84136dec77b10b2afe8893403bd97c188a0
Juho Mäkinen <juho.makinen@applifier.com>
2017-06-01 13:29:29 +0300

    Fix build.sh

--- scripts/test.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -v ${PWD}:/go/src/github.com/UnityTech/kubesourcedestcheck -w /go/src/github.com/UnityTech/kubesourcedestcheck golang /bin/bash -c "go get -v github.com/kardianos/govendor && govendor sync && go test -v "
-
